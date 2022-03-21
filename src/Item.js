d0720e0115e195bc4489e3ba04faa95af7bac5be
Venturing Guy <LVentura335@Gmail.com>
2022-03-21 10:08:17 -0700

    Item component and style created

---
+++ src/Item.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types"
+import "/.Item.scss"
+
+const Item = ({item, onAddToCart}) => (
+  <div className="Item">
+    <div className="Item__left">
+      <div className="Item__image"/>
+      <div className="Item__title">
+        {item.name}
+      </div>
+      <div className="Item__description">
+        {item.description}
+      </div>
+    </div>
+    <div className="Item__right">
+      <div className="Item__price">
+        {item.price}
+      </div>
+      <button className="Item__add-to-cart">
+        Add to cart
+      </button>
+    </div>
+  </div>
+)
+
+Item.propTypes = {
+  item: PropTypes.object.isRequired,
+  onAddToCart: PropTypes.func.isRequired
+}
+
+export default Item
