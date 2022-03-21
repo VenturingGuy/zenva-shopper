1bde2b3a22685648d4d236ce4e91c459566c0183
Venturing Guy <LVentura335@Gmail.com>
2022-03-20 22:02:29 -0700

    added hooks, created Content function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss'
 import Nav from './Nav'
 
+const Content = ({tab}) =>{
+    switch(tab) {
+      default:
+      case 'items':
+        return <span>Items</span>
+      case 'cart':
+        return <span>Cart</span>
+    }
+}
+
 const App = () => {
+  const [activeTab, setActiveTab] = useState(false)
   return (
-    <div className='App'>
-      <Nav/>
+    <div className="App">
+      <Nav
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
       <main className="App__content">
         <span>Empty</span>
       </main>
