f45913daa1f4d784fc1b460e29bb33fc3db0bf89
Venturing Guy <LVentura335@Gmail.com>
2022-03-20 20:56:06 -0700

    created components, nav component established

---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+const Nav = () =>{
+  return(
+    <nav className="App__nav">
+      <ul>
+        <li className="App__nav-item">
+          <button>Items</button>
+        </li>
+        <li className="App__nav-item">
+          <button>Cart</button>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
